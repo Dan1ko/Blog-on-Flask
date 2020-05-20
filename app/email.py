a8fff9f44bc877cf74feb1a4371cffbf2dfebc60
Dan1ko <daniko.sm@gmail.com>
2020-05-20 13:25:01 +0600

    first commit

---
+++ app/email.py
@@ -0,0 +1,20 @@
+from flask_mail import Message
+from app import mail
+from threading import Thread
+from flask_babel import _
+from flask import current_app
+
+
+def send_async_mail(app, msg):
+    with app.app_context():
+        mail.send(msg)
+
+
+def send_mail(subject, sender, recipients, text_body, html_body):
+    msg = Message(subject, sender=sender, recipients=recipients)
+    msg.body=text_body
+    msg.html=html_body
+    mail.send(msg)
+    Thread(target=send_async_mail, args=(current_app._get_current_object, msg)).start()
+
+
---
+++ config.py
@@ -0,0 +1,25 @@
+import os
+from os import environ
+
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+
+class Config(object):
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'ku7trwcbelsawuehniavuwefhiaukb'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
+        'sqlite:///' + os.path.join(basedir, 'app.db')
+    SQLALCHEMY_TRACK_MODIFICATIONS =  False
+
+    # SMTP config
+    MAIL_SERVER = environ.get('MAIL_SERVER')
+    MAIL_PORT = int(environ.get('MAIL_PORT')or 25)
+    MAIL_USE_TLS = environ.get('MAIL_USE_TLS') is not None
+    MAIL_USERNAME = environ.get('MAIL_USERNAME')
+    MAIL_PASSWORD = environ.get('MAIL_PASSWORD')
+    ADMINS = ['email']
+    POSTS_PER_PAGE = 25
+    LANGUAGES = ['en', 'ru']
+
+    # ElsaticSearch configuration
+    ELASTICSEARCH_URL = environ.get('ELASTICSEARCH_URL')
